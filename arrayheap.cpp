91ad4debb11540fb4cf5c31317888d39596b068a
Fabricio <fabriciopolicarpo0@gmail.com>
2020-11-23 01:00:16 +0000

    c++

--- arrayheap.cpp
+++ arrayheap.cpp
@@ -9,4 +9,11 @@ void arrayheap(){
   p[0] = 1; p[1] = 15; p[2] =20; p[3] =34; p[4] = 60;
   for(int i =0; i < 5; i++)
     cout<<p[i]<<endl;
+  
+  // equivalent of malloc in c++
+  int *x;
+  x = new int[2];
+  x[0] =40; x[1] =34;
+   for(int i =0; i < 2; i++)
+    cout<<x[i]<<endl;
 }
