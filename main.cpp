7b74911b5f7604b35a45e99801e82595ab498ebf
Fabricio <fabriciopolicarpo0@gmail.com>
2020-11-23 00:33:45 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "pointer.h"
+using namespace std;
+
+
+int main() {
+  int n;
+  cout<<"Enter size?";
+  cin>>n;
+  int variable[n];
+  int A[10] = {1,2,3,4,5,6,7,8,9};
+  for (int x:variable) {
+      cout<<x<<endl;
+  }
+  pointer();
+  return 0;
+}
---
+++ pointer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void pointer()
+{   
+    int a = 10;
+    int *p;
+    cout<<"______ pointer function"<<endl;
+    cout<<a<<endl;
+    // return void;
+}
