25e1936357bcd6674c7b4bfd7451bdc7dfe21d7a
Fabricio <fabriciopolicarpo0@gmail.com>
2020-11-28 01:12:39 +0000

    iteration

--- parameter_array.cpp
+++ parameter_array.cpp
@@ -10,7 +10,22 @@ int* fun(int n)
   return (p);
 }
 
+void funner(int A[ ], int n){
+  for(int i =0;  i < n; i ++){
+    cout<<A[i]<< " funner" <<endl;
+  }
+
+}
+
 int array_returned(){
+
+  int B[ ] = {2,3,4,5,6,7};
+  int n = 6;
+  funner(B, n);
+
+  for(int x:B)
+  cout<<x<<" end of line"<<endl;
+
   int *A;
   A = fun(5);
   cout<<A<<endl;
